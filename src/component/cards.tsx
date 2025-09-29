60edfc0c5d1e1201488a90c3330229887d010720
Navid Neisi <navidneisi@gmail.com>
2025-09-29 10:47:45 +0330

    refactor: improved bills placement ui

--- src/component/cards.tsx
+++ src/component/cards.tsx
@@ -13,16 +13,16 @@ const CardsComponent = ({ count, bill }: CardsProps) => {
   return (
     <div
       key={bill}
-      className="relative flex flex-col items-center justify-start rounded-2xl border border-gray-200 bg-white shadow-sm p-6 hover:shadow-md transition"
+      className="relative flex flex-col items-center justify-center rounded-2xl border border-gray-200 bg-white shadow-sm p-6 hover:shadow-md transition"
     >
-      <span className="text-3xl font-semibold mb-2">{bill}</span>
-      <span className="text-xl font-medium text-gray-700">{count}</span>
-
-      <div className="relative w-20 h-40 mt-6">
+      <div className="relative flex justify-center items-center w-full h-15 mb-4">
         {renderBills.map((_, index) => (
           <Bill key={index} index={index} bill={bill} />
         ))}
       </div>
+      <span className="text-3xl font-semibold mb-2">{bill}</span>
+      <span className="text-xl font-medium text-gray-700">{count}</span>
+
     </div>
   )
 }
