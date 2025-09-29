b0a0f465968911d6ffb72d3d96326f570b2a2b8e
Navid Neisi <navidneisi@gmail.com>
2025-09-29 10:45:40 +0330

    fix: gh_pages assets reference

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/BillExchange',
+  base: '/BillExchange/',
   plugins: [
     tailwindcss(),
     react()
